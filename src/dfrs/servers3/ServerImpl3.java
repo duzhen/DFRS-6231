afd4827d91c11b9db59f1813e916345a152833ca
Zhen Du <jenkin.du@gmail.com>
2016-11-28 03:40:20 -0500

    3.3 add getBookedFlightCount 3.4 add editFlightRecord 3.5 add transferReservation

--- src/dfrs/servers3/ServerImpl3.java
+++ src/dfrs/servers3/ServerImpl3.java
@@ -148,25 +148,102 @@ public class ServerImpl3 extends ServerInterfacePOA {
 		} else if(Config.ECONOMY_CLASS.equals(flightClass)) {
 			flightClass = "economy";
 		}
-		return impl.bookFlight(departure, firstName, lastName, address, phoneNumber, des, flightClass, flightDate);
+		String result = impl.bookFlight(departure, firstName, lastName, address, phoneNumber, des, flightClass, flightDate);
+		if(result != null&&result.contains("success")) {
+			return Config.SUCCESS;
+		} else {
+			return Config.FAIL;
+		}
 	}
 
 	@Override
 	public String getBookedFlightCount(String managerID, String recordType) {
-		// TODO Auto-generated method stub
-		return null;
+		//didn't implement by type
+//		int type = 0;
+//		if(Config.FIRST_CLASS.equals(recordType)) {
+//			type = 1;
+//		} else if(Config.BUSINESS_CLASS.equals(recordType)) {
+//			type = 2;
+//		} else if(Config.ECONOMY_CLASS.equals(recordType)) {
+//			type = 3;
+//		} else if(Config.ALL_CLASS.equals(recordType)) {
+//			type = 0;
+//		}
+		//return WST$0$WDC$0$NDL$0
+		return impl.getBookedFlightCount(impl.location, managerID);
 	}
 
 	@Override
 	public String editFlightRecord(String managerID, String recordID, String fieldName, String newValue) {
-		// TODO Auto-generated method stub
-		return null;
+		String value[] = new String[2];//destination, date
+		int seats[] = new int[3];//economy, business, firstclass
+		String v = flight.get(recordID);
+		if(v== null) {
+			return Config.FAIL;
+		} else {
+			String destination = v.substring(0, 3);
+			String flightDate = v.substring(3);
+			String keyManager = destination+flightDate;
+			if(impl.planeMap.containsKey(keyManager)) {
+				managerRecord record = impl.planeMap.get(keyManager);
+				value[0] = record.destination;
+				value[1] = record.flightDate;
+				seats[0] = record.economy;
+				seats[1] = record.business;
+				seats[2] = record.firstclass;
+			}
+		}
+		//NOT Support edit destination and date
+		if(Config.DATE.equals(fieldName)) {
+			value[1] = newValue;
+		} else if(Config.DESTINATION.equals(fieldName)) {
+			String des = Utils.getServer(newValue);
+			if(BaseServerCluster.SERVER_MTL.equals(des)) {
+				value[0] = "MTL";
+			} else if(BaseServerCluster.SERVER_WST.equals(des)) {
+				value[0] = "WST";
+			} else if(BaseServerCluster.SERVER_NDL.equals(des)) {
+				value[0] = "NDL";
+			}
+		} else if(Config.FIRST_CLASS.equals(fieldName)) {
+			seats[2] = Integer.valueOf(newValue);
+		} else if(Config.BUSINESS_CLASS.equals(fieldName)) {
+			seats[1] = Integer.valueOf(newValue);
+		} else if(Config.ECONOMY_CLASS.equals(fieldName)) {
+			seats[0] =Integer.valueOf(newValue);
+		}
+		String result = impl.editFlightRecord(impl.location, managerID, value[0], value[1], seats[0], seats[1], seats[2]);
+		if(result != null&&result.contains("success")) {
+			return Config.SUCCESS;
+		} else {
+			return Config.FAIL;
+		}
 	}
 
 	@Override
 	public String transferReservation(String managerID, String passengerID, String currentCity, String otherCity) {
-		// TODO Auto-generated method stub
-		return null;
+		String c = Utils.getServer(currentCity);
+		String o = Utils.getServer(currentCity);
+		if(BaseServerCluster.SERVER_MTL.equals(c)) {
+			currentCity = "MTL";
+		} else if(BaseServerCluster.SERVER_WST.equals(c)) {
+			currentCity = "WST";
+		} else if(BaseServerCluster.SERVER_NDL.equals(c)) {
+			currentCity = "NDL";
+		}
+		if(BaseServerCluster.SERVER_MTL.equals(o)) {
+			otherCity = "MTL";
+		} else if(BaseServerCluster.SERVER_WST.equals(o)) {
+			otherCity = "WST";
+		} else if(BaseServerCluster.SERVER_NDL.equals(o)) {
+			otherCity = "NDL";
+		}
+		String result = impl.transferReservation(managerID, passengerID, currentCity, otherCity);
+		if(result != null&&result.contains("succeed")) {
+			return Config.SUCCESS;
+		} else {
+			return Config.FAIL;
+		}
 	}
 
 }
